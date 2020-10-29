4e102878b8374377dec8f91ee1c0e00fc0f65ab2
Venom <thetrueVenom@gmx.de>
2020-10-29 20:35:15 +0100

    test

--- 02_employee/src/com/company/Main.java
+++ 02_employee/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("HELLO");
+
+        System.out.println("What up my brotha");
+
     }
+
 }
