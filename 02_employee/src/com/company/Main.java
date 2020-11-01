8b579febffda9e8c543e3c9f34615b8d6af6172a
Venom <thetruevenom@gmx.de>
2020-11-01 15:28:29 +0100

    implemented Employee class

--- 02_employee/src/com/company/Main.java
+++ 02_employee/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HELLO");
+
+        Employee carl = new Employee("carl");
+
+        Employee sasha = new Employee("sasha", 2000);
+
+        carl.setSalary(4000);
+
+        sasha.setSalary(51000);
+
+        System.out.println(carl.getEmployeeInfo());
+        System.out.println(sasha.getEmployeeInfo());
+
     }
 }
