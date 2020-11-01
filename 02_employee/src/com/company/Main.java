7bf4cead9aa933e7d593748b9b8270e07317b7af
Venom <thetruevenom@gmx.de>
2020-11-01 15:38:17 +0100

    corrected merging errors

--- 02_employee/src/com/company/Main.java
+++ 02_employee/src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         Employee carl = new Employee("carl");
 
         Employee sasha = new Employee("sasha", 2000);
@@ -15,11 +14,6 @@ public class Main {
 
         System.out.println(carl.getEmployeeInfo());
         System.out.println(sasha.getEmployeeInfo());
-=======
-        System.out.println("HELLO");
-
-        System.out.println("What up my brotha");
->>>>>>> 9fca37126b4622a4ebd8877bfa00e5a5e57b1218
 
     }
 
